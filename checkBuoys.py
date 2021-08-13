9f08de398fae360f5347c6f0dcf39f5ea00f2683
Ricardo M Campos <rcamp@Orion-login-1.HPC.MsState.Edu>
2021-08-13 14:24:03 -0500

    check and compare differences between Buoy positions in the GFS e GEFS lists, create a log file

--- checkBuoys.py
+++ checkBuoys.py
@@ -5,7 +5,7 @@ from pylab import *
 from mpl_toolkits.basemap import Basemap
 
 # Read NDBC buoys, lat/lon and buoyID
-dfgefs = pd.read_csv("wave_gefs.buoys", comment='$')
+dfgefs = pd.read_csv("wave_gefs.buoys", comment='$', header=None)
 bposgefs=np.zeros((dfgefs.values.shape[0]-1,2),'float')*np.nan; bidgefs=[]
 for i in range(0,dfgefs.values.shape[0]-1):
 	aux=np.str(dfgefs.values[i]).split()
@@ -13,7 +13,7 @@ for i in range(0,dfgefs.values.shape[0]-1):
 	bposgefs[i,1]=np.float(aux[1]) # lon
 	bidgefs=np.append(bidgefs,aux[3][1::]) # ndbc buoy ID
 
-dfgfs = pd.read_csv("wave_gfs.buoys.dat", comment='$')
+dfgfs = pd.read_csv("wave_gfs.buoys.dat", comment='$', header=None)
 bposgfs=np.zeros((dfgfs.values.shape[0]-1,2),'float')*np.nan; bidgfs=[]
 for i in range(0,dfgfs.values.shape[0]-1):
 	aux=np.str(dfgfs.values[i]).split()
@@ -21,7 +21,42 @@ for i in range(0,dfgfs.values.shape[0]-1):
 	bposgfs[i,1]=np.float(aux[1]) # lon
 	bidgfs=np.append(bidgfs,np.str(aux[3][1::]).split("'")[0])
 
-dfabs = pd.read_csv("allbstations.dat", comment='$')
+
+# Check if all GEFS buoys are contained in GFS list
+for i in range(0,size(bidgefs)):
+	ind = np.where( bidgfs == bidgefs[i])
+	if np.any(ind)==True or ind==0:
+		print('  GEFS Buoy '+bidgefs[i]+' in the GFS list')
+		if np.abs(bposgfs[ind[0][0],0] - bposgefs[i,0]) + np.abs(bposgfs[ind[0][0],1] - bposgefs[i,1]) > 0.2:
+			print(' '); print('Warning at '+bidgefs[i]+' Positions do not match')
+			print(" LATgfs: "+repr(bposgfs[ind[0][0],0])+"  LONgfs: "+repr(bposgfs[ind[0][0],1]))
+			print(" LATgefs: "+repr(bposgefs[i,0])+"  LONgefs: "+repr(bposgefs[i,1]))
+			print(' ')	
+			
+	else:
+		print(' '); print('GEFS Buoy '+bidgefs[i]+' is NOT contained in GFS list'); print(' ')
+
+# -----------------------
+
+
+
+# Check if GFS buoys are contained in GEFS list
+for i in range(0,size(bidgfs)):
+	ind = np.where( bidgefs == bidgfs[i])
+	if np.any(ind)==True or ind==0:
+		print('  GEFS Buoy '+bidgfs[i]+' in the GFS list')
+		if np.abs(bposgefs[ind[0][0],0] - bposgfs[i,0]) + np.abs(bposgefs[ind[0][0],1] - bposgfs[i,1]) > 0.2:
+			print(' '); print('Warning at '+bidgfs[i]+' Positions do not match')
+			print(" LATgefs: "+repr(bposgefs[ind[0][0],0])+"  LONgefs: "+repr(bposgefs[ind[0][0],1]))
+			print(" LATgfs: "+repr(bposgfs[i,0])+"  LONgfs: "+repr(bposgfs[i,1]))
+			print(' ')
+
+	else:
+		print(' '); print('  GEFS Buoy '+bidgfs[i]+' is NOT contained in GEFS list'); print(' ')
+
+
+
+dfabs = pd.read_csv("allbstations.dat", comment='$', header=None)
 bposabs=np.zeros((dfabs.values.shape[0]-1,2),'float')*np.nan; bidabs=[]
 for i in range(0,dfabs.values.shape[0]-1):
 	aux=np.str(dfabs.values[i]).split()
@@ -29,23 +64,6 @@ for i in range(0,dfabs.values.shape[0]-1):
 	bposabs[i,1]=np.float(aux[1]) # lon
 	bidabs=np.append(bidabs,np.str(aux[3][1::]).split("'")[0])
 
-
-# Check if all GEFS buoys are contained in GFS list
-for i in range(0,size(bidgefs)):
-	ind = np.where( bidgfs == bidgefs[i])
-	if np.any(ind)==True or ind==0:
-		print('Buoy '+bidgefs[i]+' Ok')
-	else:
-		print('Buoy '+bidgefs[i]+' is not contained in GFS list')
-
-
-# Check if GFS buoys are contained in GEFS list
-for i in range(0,size(bidgfs)):
-	ind = np.where( bidgefs == bidgfs[i])
-	if np.any(ind)==True or ind==0:
-		print('Buoy '+bidgfs[i]+' Ok')
-	else:
-		print('Buoy '+bidgfs[i]+' is not contained in GEFS list')
 
 
 # Plot positions
